27966ecaf9d501d706e53ed5ef7d64f76f11d6ae
Melody P. <melpatarodi@gmail.com>
2026-09-08 12:28:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,39 @@
-//wip
+#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    vector <float>grades;
+    int size;
+    float grade;
+    
+    cout << "How many students are there?: ";
+    cin >> size;
+
+    for (int i = 0; i < size; i++) {
+        cout << "Enter grade " << i+1 << ": ";
+        cin >> grade;
+        grades.push_back(grade);
+    }
+
+    float average = 0;
+    float lowest = grades[0];
+    float highest = grades[0];
+
+    for (int i = 0; i < size; i++) {
+        average+=grades[i];
+        if (grades[i] < lowest) {
+            lowest = grades[i];
+        }
+        if (grades[i] > highest) {
+            highest  = grades[i];
+        }
+    }
+    cout << "===============================\n";
+    cout << "The average grade is: " << fixed << setprecision(1) <<  average / size << "\n";
+    cout << "The lowest grade is: " << lowest << "\n";
+    cout << "The highest grade is: " << highest << "\n";
+
+    return 0;
+}
